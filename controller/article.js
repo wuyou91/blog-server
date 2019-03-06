725f44caf7aacff04bd5a983829af42af3d1cc9a
Changxing.Yan <ycx1991@live.com>
2019-03-06 18:44:07 +0800

    文章存入数据库

--- controller/article.js
+++ controller/article.js
@@ -2,10 +2,31 @@ const formidable = require('formidable')
 const articleModel = require('../mongodb/models/article.js')
 
 module.exports = {
-  add (req, res) {
+  async add (req, res) {
     const form = new formidable.IncomingForm()
-    form.parse(req, (err, fields, files) => {
-      res.send(fields)
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        res.send({
+          status: 0,
+					message: '表单信息错误'
+        })
+        return
+      }
+      if(!fields.html || fields.html === ''){
+        res.send({
+          status: 0,
+					message: '请勿提交无内容的文章'
+        })
+        return
+      }
+      await articleModel.create({
+        ...fields,
+        create_time: new Date().toLocaleString()
+      })
+      res.send({
+        status: 1,
+        message: '文章提交成功'
+      })
     })
   }
 }
