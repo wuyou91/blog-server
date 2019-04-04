cec46e0f5df37aa59907b98812b8bed8b9d13f37
wuyou <ycx1991@live.com>
2019-04-05 01:42:59 +0800

    xiugai

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ require('./mongodb/db.js')
 const app = express()
 
 app.all('*', (req, res, next) => {
-  const { origin, Origin, referer, Referer } = req.headers;
-  const allowOrigin = origin || Origin || referer || Referer || '*';
-	res.header("Access-Control-Allow-Origin", allowOrigin);
+  // const { origin, Origin, referer, Referer } = req.headers;
+  // const allowOrigin = origin || Origin || referer || Referer || '*';
+	res.header("Access-Control-Allow-Origin", "admin.yancx.cn");
 	res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
 	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Credentials", true); //可以带cookies
