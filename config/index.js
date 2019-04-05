9975ea432452a3c0f653919b42da8fc5f185969d
wuyou <ycx1991@live.com>
2019-04-05 12:16:26 +0800

    cookie设置

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ module.exports = {
     name: 'sid',
     secret: 'sid',
     cookie:{
-        domain:'admin.yancx.cn',
+        domain:'.yancx.cn',
         httpOnly: true,
         secure:   false,
         maxAge:   1000 * 60 * 60 * 24 * 365 // 1年
